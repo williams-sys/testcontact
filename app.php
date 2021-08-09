86495bac77e481e47ffe206407d1512fb7d25201
Williams Shann Htet <htetshan39@gmail.com>
2021-08-09 19:43:40 +0630

    test

--- app.php
+++ app.php
@@ -1,27 +1,17 @@
 
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $mailForm = $_POST['mail'];
+    $message = $_POST['message'];
 
+    $mailTo = "shanhtet07@gmail.com";
+    $headers = "Form: ".$mailForm;
+    $txt = "You have a message ".$name .$message;
 
+    mail($mailTo, $name, $txt, $headers);
 
-$email_from = 'william-shann-htet.com';
-
-$email_subject = "New Form Submission";
-
-$email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-                    "user Message: $message.\n";
-
-$to = "shanhtet07@gmail.com";
-
-$headers = "From: $email_from \r\n";
-
-$headers .= "Reply-To: $visitor_email \r\n";
-
-mail($to, $email_subject, $email_body, $headers);
-
-header("Location: index.html");
+    header("Location: index.html?MessageSent");
+}
 
 ?>
